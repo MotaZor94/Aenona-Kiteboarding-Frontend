326da6fae852e1743612a9880662830f3fca1f0f
Petr Stoklas <petrstoklas@gmail.com>
2019-12-14 11:05:37 +0100

    WhiteBox

--- src/components/WhiteBox/StyledNav.js
+++ src/components/WhiteBox/StyledNav.js
@@ -1,5 +1,5 @@
-import styled, { css } from "styled-components"
-import { theme } from "../../../theme"
+import styled, { css } from 'styled-components'
+import { theme } from '../../../theme'
 
 const WhiteBox = styled.div`
   display: flex;
@@ -13,8 +13,7 @@ const WhiteBox = styled.div`
   /* box-shadow: 1px 0px 5px 0px rgba(0, 0, 0, 0.25); */
   z-index: 20;
   height: 3.6em;
-  ${({ top }) =>
-    top
+  ${({ top }) => top
       ? css`
           top: 0;
           border-bottom: 0.5px solid ${theme.colors.palette.blue.normal};
@@ -25,6 +24,4 @@ const WhiteBox = styled.div`
         `}
 `
 
-export const Styled = {
-  WhiteBox,
-}
+export default WhiteBox
--- src/components/WhiteBox/WhiteBox.js
+++ src/components/WhiteBox/WhiteBox.js
@@ -1,13 +1,13 @@
-import React from "react"
-import { Styled } from "./StyledNav"
-import Image from "../Image/Image"
-import Logo from "../../static/logo_vert_w.png"
+import React from 'react'
+import StyledWhiteBox from './StyledNav'
+import Image from '../Image/Image'
+import Logo from '../../static/logo_vert_w.png'
 
 const WhiteBox = () => {
   return (
-    <Styled.WhiteBox>
+    <StyledWhiteBox>
       <Image src={Logo} />
-    </Styled.WhiteBox>
+    </StyledWhiteBox>
   )
 }
 
