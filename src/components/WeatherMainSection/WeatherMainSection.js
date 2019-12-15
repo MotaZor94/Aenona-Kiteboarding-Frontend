203a1a5edd05d0411296f1a067073903fa2867ef
Petr Stoklas <petrstoklas@gmail.com>
2019-12-15 11:39:25 +0100

    Cleaning.

--- src/components/WeatherMainSection/WeatherMainSection.js
+++ src/components/WeatherMainSection/WeatherMainSection.js
@@ -4,8 +4,6 @@ import MainImgWrap from '../MainImgWrap/MainImgWrap'
 import MainPanel from '../MainPanel/MainPanel'
 import WeatherImgAndTemp from '../WeatherImgAndTemp/WeatherImgAndTemp'
 
-// TODO: Not displaying mainImgWrap properly
-
 const WeatherMainSection = () => {
   return (
     <MainSection weather>
