65a29a9aac41b573e18019bb45b1363ea448f7cf
Marko <colicmarko94@gmail.com>
2019-12-16 22:13:43 +0100

    gitflow-feature: Newlogo
    Added new logo to whitebox and moved image component outside of it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
-
+import Logo from "../static/Tekst.png"
 // will go as prop
 
 // import Img from "gatsby-image"
@@ -13,14 +13,17 @@ import HomeMainSection from "../components/HomeMainSection/HomeMainSection"
 
 import WeatherMainSection from "../components/WeatherMainSection/WeatherMainSection"
 import WhiteBox from "../components/WhiteBox/WhiteBox"
-
+import Image from "../components/Image/Image"
+import LogoText from "../static/Tekst.png"
 const IndexPage = () => {
   return (
     <>
       <SEO title="Home" />
-      <WhiteBox top />
+      <WhiteBox top>
+        <Image src={LogoText} width={"120px"} />
+      </WhiteBox>
       <HomeMainSection />
-      <WhiteBox />
+      <WhiteBox></WhiteBox>
       <Link to="/">Go to page 2</Link>
     </>
   )
