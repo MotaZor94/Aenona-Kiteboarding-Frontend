5e697b64ed0655bc44c825a05d554edc011ee8f9
Petr Stoklas <petrstoklas@gmail.com>
2020-01-02 13:55:55 +0100

    Wheather hourly.

--- src/components/WeatherHourly/WeatherHourly.js
+++ src/components/WeatherHourly/WeatherHourly.js
@@ -1,8 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Text from '../Text/Text'
+import WeatherIcon from '../WeatherIcon/WeatherIcon'
+import Card from '../Card/Card'
 
-const WeatherHourly = props => {
-  return <div>Weather Hourly</div>
+const WeatherHourly = ({ data }) => {
+  return (
+    <Card flexDirection="row" margin="5px" scroll="x">
+      {data.map(item => (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: '5px 15px',
+          }}
+        >
+          <Text isBold as="weatherData">
+            {item.time}
+          </Text>
+          <WeatherIcon type="clearNight" />
+          <Text isBold as="weatherData">
+            {`${item.temp} C`}
+          </Text>
+          <Text as="weatherData">{`${item.windSpeed} kn`}</Text>
+        </div>
+      ))}
+    </Card>
+  )
 }
 
 WeatherHourly.propTypes = {}
