0c135cda6d999c746bf595e2ae939601f368ec97
Petr Stoklas <petrstoklas@gmail.com>
2020-01-02 13:53:08 +0100

    Switch cases added.

--- src/components/WeatherIcon/WeatherIcon.js
+++ src/components/WeatherIcon/WeatherIcon.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import windIcon from '../../images/icons/wind.png'
+import nightIcon from '../../images/icons/night.png'
 import Image from '../Image/Image'
 
 const WeatherIcon = ({ type }) => {
   let icon
   switch (type) {
+    case 'clearNight':
+      icon = nightIcon
+      break
+
     default:
       icon = windIcon
-      console.log(icon)
+      break
   }
   console.log(icon)
   return <Image src={icon} width="28px" />
