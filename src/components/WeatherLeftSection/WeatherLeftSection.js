1dae6af8f3bebb2c09977e2284f496150bdb17f4
Petr Stoklas <petrstoklas@gmail.com>
2020-01-02 13:53:29 +0100

    Dummy data 1

--- src/components/WeatherLeftSection/WeatherLeftSection.js
+++ src/components/WeatherLeftSection/WeatherLeftSection.js
@@ -3,10 +3,43 @@ import PropTypes from 'prop-types'
 import WeatherHourly from '../WeatherHourly/WeatherHourly'
 import WeatherDaily from '../WeatherDaily/WeatherDaily'
 
+const dummyData = [
+  {
+    time: 22,
+    temp: 23.1,
+    windSpeed: 8.2,
+  },
+  {
+    time: 22,
+    temp: 23.1,
+    windSpeed: 8.2,
+  },
+  {
+    time: 22,
+    temp: 23.1,
+    windSpeed: 8.2,
+  },
+  {
+    time: 22,
+    temp: 23.1,
+    windSpeed: 8.2,
+  },
+  {
+    time: 22,
+    temp: 23.1,
+    windSpeed: 8.2,
+  },
+  {
+    time: 22,
+    temp: 23.1,
+    windSpeed: 8.2,
+  },
+]
+
 const WeatherLeftSection = props => {
   return (
     <div>
-      <WeatherHourly />
+      <WeatherHourly data={dummyData} />
       <WeatherDaily />
     </div>
   )
