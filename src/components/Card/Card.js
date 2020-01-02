77ae3f439858f2ae76e0c654dd36cc12c006e18d
Petr Stoklas <petrstoklas@gmail.com>
2020-01-02 13:47:51 +0100

    New props for Card.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,12 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import StyledCard from './StyledCard'
 
-const Card = ({ children }) => {
-  return <StyledCard>{children}</StyledCard>
+const Card = ({ children, flexDirection, margin, scroll, padding }) => {
+  return (
+    <StyledCard
+      flexDirection={flexDirection}
+      margin={margin}
+      scroll={scroll}
+      padding={padding}
+    >
+      {children}
+    </StyledCard>
+  )
 }
 
 Card.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  flexDirection: PropTypes.string,
+  margin: PropTypes.string,
+  scroll: PropTypes.string,
+  padding: PropTypes.string,
 }
 
 export default Card
