57904e391bfd72864eb324060d4e95c3e20f73bb
Petr Stoklas <petrstoklas@gmail.com>
2020-01-02 13:59:36 +0100

    StyledWeatherDayWrap.

--- src/components/WeatherDaily/WheatherDay.js
+++ src/components/WeatherDaily/WheatherDay.js
@@ -2,18 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Text from '../Text/Text'
 import WeatherIcon from '../WeatherIcon/WeatherIcon'
+import StyledWeatherDayWrap from './StyledWeatherDayWrap'
 
 const WheatherDay = props => {
   return (
-    <div>
-      <Text as="weatherData">Monday</Text>
+    <StyledWeatherDayWrap>
+      <div style={{ flexGrow: '1' }}>
+        <Text as="weatherData">Monday</Text>
+      </div>
       <div>
         <WeatherIcon />
         <Text as="weatherData">22 C</Text>
         <WeatherIcon />
         <Text as="weatherData">8.1</Text>
       </div>
-    </div>
+    </StyledWeatherDayWrap>
   )
 }
 
