45996efd23ab5f3a623dd4ee541e551752a499ad
Petr Stoklas <petrstoklas@gmail.com>
2020-01-26 12:51:15 +0100

    media query for centering the weather component.

--- src/components/MainImgWrap/StyledMainImgWrap.js
+++ src/components/MainImgWrap/StyledMainImgWrap.js
@@ -8,10 +8,12 @@ export default styled.div`
           padding: 15px;
           padding-top: 30px;
           max-width: 500px;
-          margin: auto;
           overflow: hidden;
           @media (min-width: 800px) {
             width: 400px;
+          }
+          @media (max-width: 800px) {
+            margin: auto;
           }
         `
       : css`
